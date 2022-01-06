505abc484110764780debd32474c9d67765abf67
aniketcodes <aniketsinha1304@gmail.com>
2022-01-06 13:22:31 +0530

    client side script for socket.io

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const express = require( 'express' )
 const app = express();
 const path = require( "path" );
 const http = require( "http" );
+const socketio = require( "socket.io" );
 const PORT = process.env.PORT;
 const server = http.createServer( app );
+const io = socketio( server );
+
 app.use("/",express.static(path.join(__dirname,"public")))
+
+io.on( 'connection', () => {
+  console.log("New web socket connection")
+})
 
 //LISTEN Route
 server.listen( PORT, ( err ) => {
