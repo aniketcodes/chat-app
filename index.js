256fd0fec1e92fa6d733914c17f7759cb2e4acc6
aniketcodes <aniketsinha1304@gmail.com>
2022-01-07 01:16:56 +0530

    timestamp

--- index.js
+++ index.js
@@ -8,26 +8,26 @@ const socketio = require( "socket.io" );
 const PORT = process.env.PORT;
 const server = http.createServer( app );
 const io = socketio( server );
-
+const {generateLocationMessage,generateMessage} = require( "./src/utils/messages" );
 app.use("/",express.static(path.join(__dirname,"public")))
 
 
 io.on( 'connection', (socket) => {
   console.log( "New web socket connection" )
-  socket.emit( 'message', "Welcome" );
-  socket.broadcast.emit( 'message', "A new user has joined" );
+  socket.emit( 'message',generateMessage("Welcome") );
+  socket.broadcast.emit( 'message', generateMessage("A new user joined") );
   socket.on( "sendMessage", ( message ,cb) => {
-    io.emit( "message", message );
+    io.emit( "message", generateMessage( message) );
     cb();
   } );
 
   socket.on( "sendLocation", ( { latitude, longitude },cb ) => {
-    socket.broadcast.emit( "locationMessage", `https://maps.google.com?q=${ latitude },${ longitude }` )
+    socket.broadcast.emit( "locationMessage", generateLocationMessage( `https://maps.google.com?q=${ latitude },${ longitude }` ))
     cb();
   })
 
   socket.on( "disconnect", () => {
-    io.emit( "message", "A user has left the chat" );
+    io.emit( "message", generateMessage( "A user has left the chat") );
   })
 })
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,51 +1,57 @@
 const socket = io()
 
-let $messageForm = document.querySelector( "#message-form" );
+let $messageForm = document.querySelector( "#message-form" )
 let $messageFormInput = $messageForm.querySelector( "input" )
-let $messageFormButton = $messageForm.querySelector( "button" );
+let $messageFormButton = $messageForm.querySelector( "button" )
 
-let $locationButton = document.querySelector( "#send-location" );
-let $messages = document.querySelector( "#messages" );
+let $locationButton = document.querySelector( "#send-location" )
+let $messages = document.querySelector( "#messages" )
 
 //Template
 
-let messageTemplate = document.querySelector( "#message-template" ).innerHTML;
-let locationTemplate = document.querySelector( "#location-message-template" ).innerHTML;
+let messageTemplate = document.querySelector( "#message-template" ).innerHTML
+let locationTemplate = document.querySelector( "#location-message-template" ).innerHTML
 
-socket.on( 'message', (message) => {
-  console.log( message );
+socket.on( 'message', ( message ) => {
+  console.log( message )
 
-  let html = Mustache.render( messageTemplate ,{message});
-  $messages.insertAdjacentHTML('beforeend',html)
+  let html = Mustache.render( messageTemplate, {
+    message: message.text,
+    createdAt: moment( message.createdAt ).format( 'h:mm a' )
+  } )
+  $messages.insertAdjacentHTML( 'beforeend', html )
 } )
 
-socket.on( 'locationMessage', ( url ) => {
-  let html = Mustache.render( locationTemplate, { url } );
-  $messages.insertAdjacentHTML( "beforeend", html );
-})
+socket.on( 'locationMessage', ( location ) => {
+  let html = Mustache.render( locationTemplate, {
+    url: location.url,
+    createdAt: moment( location.createdAt ).format( 'h:mm a' )
+  } )
+  $messages.insertAdjacentHTML( "beforeend", html )
+} )
 
 $messageForm.addEventListener( "submit", ( e ) => {
-  e.preventDefault();
-  $messageFormButton.setAttribute("disabled","disabled")
-  let message = e.target.elements.message.value;
+  e.preventDefault()
+  $messageFormButton.setAttribute( "disabled", "disabled" )
+  let message = e.target.elements.message.value
   socket.emit( "sendMessage", message, () => {
-    $messageFormButton.removeAttribute( "disabled" );
-    $messageFormInput.value = '';
-    $messageFormInput.focus();
-    console.log("message was delivered")
-  } );
+    $messageFormButton.removeAttribute( "disabled" )
+    $messageFormInput.value = ''
+    $messageFormInput.focus()
+    console.log( "message was delivered" )
+  } )
 } )
 
 $locationButton.addEventListener( "click", () => {
   if ( !navigator.geolocation ) {
-    return alert("Geolocation is not supported by browser")
+    return alert( "Geolocation is not supported by browser" )
   }
-  $locationButton.setAttribute( "disabled", 'disabled' );
+  $locationButton.setAttribute( "disabled", 'disabled' )
   navigator.geolocation.getCurrentPosition( ( location ) => {
-    let { latitude, longitude } = location.coords;
+    let { latitude, longitude } = location.coords
     socket.emit( "sendLocation", { latitude, longitude }, () => {
       console.log( "Location shared" )
-      $locationButton.removeAttribute("disabled")
-    })
-  })
-})
+      $locationButton.removeAttribute( "disabled" )
+    } )
+  } )
+} )
